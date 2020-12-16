bea64abaf75f9f5bae10bcdbee52e97c6607b2f9
Zarrar Malik <zarrarmalik@coop.co.uk>
2020-12-16 13:12:50 +0000

    Add stripe implementation to impresso espresso app

---
+++ src/main/java/com/impresso/espresso/stripe/ImpressoEspressoStripeIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.impresso.espresso.stripe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpressoEspressoStripeIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImpressoEspressoStripeIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/impresso/espresso/stripe/ImpressoEspressoStripeIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impresso.espresso.stripe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpressoEspressoStripeIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
